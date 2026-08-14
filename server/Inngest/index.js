5b4569fe0b196baf773801e63ef6acc2699ce31a
Pritam Ahire <ahirepritam22@gmail.com>
2026-08-14 07:36:35 +0530

    inngest function upadtion fix

--- server/Inngest/index.js
+++ server/Inngest/index.js
@@ -4,7 +4,7 @@ export const inngest = new Inngest({ id: "movie-ticket-booking" });
 // Create an empty array where we'll export future Inngest functions
 
 // Saving Inngest Data to Database
- const SyncUserCreation = inngest.createFunction(
+const SyncUserCreation = inngest.createFunction(
   {
     id: "sync-user-from-clerk",
     triggers: [{ event: "clerk/user.created" }],
@@ -25,40 +25,35 @@ export const inngest = new Inngest({ id: "movie-ticket-booking" });
 );
 
 const SyncUserDeletion = inngest.createFunction(
-    {
-        id:"delete-user-with-clerk",
-        triggers:[{event:"clerk/user.deleted"}],
-    },
+  {
+    id: "delete-user-with-clerk",
+    triggers: [{ event: "clerk/user.deleted" }],
+  },
 
-    async({event})=>{
-
-        const {id} = event.data
-        await User.findByIdAndDelete(id)
-    }
-)
-
-
+  async ({ event }) => {
+    const { id } = event.data;
+    await User.findByIdAndDelete(id);
+  },
+);
 
 const SyncUserUpdation = inngest.createFunction(
-    {
-        id:"update-user-with-clerk",
-        triggers:[{event:"clerk/user.updated"}],
-    },
+  {
+    id: "update-user-with-clerk",
+    triggers: [{ event: "clerk/user.updated" }],
+  },
 
-    async({event})=>{
-        
-        const {id, first_name, last_name, email_addresses, image_url} = event.data
-        const UserData = {
-            _id:id,
-            name:first_name + ' ' + last_name,
-            emai:email_addresses[0].email_address,
-            image:image_url
-        }
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
+    const UserData = {
+      _id: id,
+      name: first_name + " " + last_name,
+      email: email_addresses[0].email_address,
+      image: image_url,
+    };
 
-await User.findByIdAndUpdate(id, user)
-    }
-)
+    await User.findByIdAndUpdate(id, user);
+  },
+);
 
-
-
-export const functions = [SyncUserCreation, SyncUserDeletion];
+export const functions = [SyncUserCreation, SyncUserDeletion, SyncUserUpdation];
